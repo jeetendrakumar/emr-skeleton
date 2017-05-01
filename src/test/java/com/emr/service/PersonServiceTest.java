f5980c37573003ee9b0ff74db305be8e37b3255f
jeetendrakumar <jeetendra.hadagali@gmail.com>
2017-05-01 19:05:15 +0530

    Adding mockito

--- src/test/java/com/emr/service/PersonServiceTest.java
+++ src/test/java/com/emr/service/PersonServiceTest.java
@@ -1,18 +1,35 @@
 package com.emr.service;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.emr.datarepository.PersonRepository;
+import com.emr.model.Person;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(PersonService.class)
 public class PersonServiceTest {
     
+    @Mock
+    private PersonRepository personRepository;
+    
+    @InjectMocks
     PersonService personService;
     
-
     @Before
-    public void setUp() throws Exception {
+    public void setup() throws Exception {
         personService = new PersonService();
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
@@ -21,7 +38,9 @@ public class PersonServiceTest {
 
     @Test
     public final void testGetPerson() {
-        assertNotNull(personService.getPerson(12));
+        Person person = new Person();
+        when(personRepository.findOne(anyLong())).thenReturn(person);
+        assertNotNull(personService.getPerson(12l));
     }
 
 }
